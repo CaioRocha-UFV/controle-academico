62d1a2fc493ed4a5dfb38873bb75e049e279b358
Maria Theresa <MARIA.THERESA@UFV.BR>
2021-04-01 11:29:12 -0300

    Update Testing.java

---
+++ Testing.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+
+import utils.*;
+
+public class Testing {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        /*
+            CLASSE MENU FOI CRIADA PARA PERMITIR TESTAR A APLICAÇÃO5
+        */
+        Menu menu = new Menu();
+        menu.mostrarMenu();    
+    }
+}
