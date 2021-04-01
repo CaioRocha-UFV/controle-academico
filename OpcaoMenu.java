f40d466ab1ecd7ae1086763d38abcd96d0b5deeb
Maria Theresa <MARIA.THERESA@UFV.BR>
2021-04-01 11:27:12 -0300

    Create OpcaoMenu.java

---
+++ OpcaoMenu.java
@@ -0,0 +1,44 @@
+package utils;
+
+public enum OpcaoMenu {
+    PESSOA ("Pessoa"),
+    ALUNO ("Aluno"),
+    PROFESSOR ("Professor"),
+    CURSO("Curso"),
+    DISCIPLINA("Disciplina"),
+    ATIVIDADECOMPLEMENTAR("Atividade Complementar"),
+    GRADE("Grade"),
+    HISTORICO("Histórico"),
+    INSTITUICAOENSINO("Instituição de Ensino"),
+    DEPARTAMENTO("Departamento"),
+    ENCERRARPEDIDO("Encerrar programa");
+
+    private final String opcao;
+    public static final int MAXOPCOES = 11;
+    public static final int MINOPCOES =1;
+
+    /*
+        CONSTRUTOR
+    */  
+    OpcaoMenu(String opcao){
+        this.opcao = opcao;
+    }
+    
+    /*
+        GETTER
+    */
+    public String getOpcao(){
+        return opcao;
+    }
+
+    /*
+        MÉTODOS ADICIONAL
+    */
+    public static void mostrarOpcoes() {
+        OpcaoMenu[] opcoes = OpcaoMenu.values();
+        for (OpcaoMenu opcao : opcoes) {
+            System.out.print(opcao.ordinal()+1 + "-" + opcao.getOpcao() + "\n");
+        }
+        System.out.println();
+    }
+}
