daf249c7eb56c0d63732e75b8e1b1f8ad5fbfadd
Maria Theresa <MARIA.THERESA@UFV.BR>
2021-04-01 11:07:36 -0300

    Update GradeCurricular.java

--- GradeCurricular.java
+++ GradeCurricular.java
@@ -1,3 +1,69 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GradeCurricular {
+    private String ano;
+    private int totalCredito;
+
+    
+    /*
+        CONSTRUTORES
+    */
+    public GradeCurricular(String ano) {
+        this.ano = ano;
+        // this.nomeDisciplina = nomeDisciplina;
+        // this.totalCredito = totalCredito;
+    }
+
+    /*
+        SETTERS
+    */
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public void setTotalCredito(int totalCredito){
+        this.totalCredito = totalCredito;
+    }
+
+    /*
+        GETTERS
+    */
+    public String getAno() {
+        return ano;
+    }
+
+    public int getTotalCredito(){
+        return totalCredito;
+    }
+
+    
+    /*
+        METODOS
+    */
+    public void cadastrarGradeCurricular(String ano){
+        // int somaCreditos = 0;
+
+        System.out.println("Disciplinas que podem ser incluidas: ");
+        // Disciplina.mostrarDisciplinas(disciplinas);
+
+        // while(this.totalCredito > somaCreditos){
+            // List<GradeCurricular> incluirDisciplina = new ArrayList<GradeCurricular>();
+            // incluirDisciplina.add(new GradeCurricular(nome, professor, qntCreditos));
+        // }
+    }
+
+    public static void mostrarGrade(List<GradeCurricular> grade) {
+        System.out.println(grade);
+    }
+
+    public String toString(){
+        return "Grade: "
+        + ano
+        +"\n";
+    }
+    
     
 }
