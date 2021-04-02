216bc890980e18154941af8c290c016f9e9cd6b9
Erian <70418490+Erian-A@users.noreply.github.com>
2021-04-01 22:39:24 -0300

    Curso e Departamento - básico implementado

--- Curso.java
+++ Curso.java
@@ -1,3 +1,81 @@
+import java.util.HashMap;
+
+import entities.GradeCurricular;
+
 public class Curso {
-    int id;
+    private String codigo;
+    private String nome;
+    private Professor coordenador;
+    private GradeCurricular gradePadrao;
+    private HashMap<String, Professor> professores = new HashMap<String, Professor>();
+    private int alunosMatriculados; // Ver se eh uma informacao cabivel no contexto presente
+
+
+    /*
+        CONSTRUTORES
+    */
+    public Curso(String id, String nome, Professor coordenador, GradeCurricular grade){
+
+        this.setCodigo(id);
+        this.setNome(nome);
+        this.setCoordenador(coordenador);
+        this.setGradePadrao(grade);
+    }
+
+    /*
+        GETTERS
+    */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Professor getCoordenador() {
+        return coordenador;
+    }
+
+    public GradeCurricular getGradePadrao() {
+        return gradePadrao;
+    }
+
+    public HashMap<String, Professor> getProfessores() {
+        return professores;
+    }
+
+    public int getAlunosMatriculados() {
+        return alunosMatriculados;
+    }
+
+
+    /*
+        SETTERS
+    */
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCoordenador(Professor coordenador) {
+        this.coordenador = coordenador;
+    }
+
+    public void setGradePadrao(GradeCurricular gradePadrao) {
+        this.gradePadrao = gradePadrao;
+    }
+
+    public void setProfessores(HashMap<String, Professor> professores) {
+        //this.professores.put(professores.getNome(), professores);
+    }
+
+    public void setAlunosMatriculados(int alunosMatriculados) {
+        this.alunosMatriculados = alunosMatriculados;
+    }
+
 }
--- Departamento.java
+++ Departamento.java
@@ -1,5 +1,83 @@
 import java.util.HashMap;
 
 public class Departamento {
-    HashMap<String, Curso> cursos = new HashMap<String, Curso>();
+    
+    private String nome;
+    private HashMap<String, Curso> cursosAssociados = new HashMap<String, Curso>();
+    private Professor diretor;
+    private String email;
+    private String telefone;
+    private String codigo;
+
+
+    /*
+        CONSTRUTORES
+    */
+
+    public Departamento(String nome, String codigo, Professor diretor, String email, String telefone){
+
+        this.setNome(nome);
+        this.setCodigo(codigo);
+        this.setDiretor(diretor);
+        this.setEmail(email);
+        this.setEmail(email);
+        this.setTelefone(telefone);
+    }
+
+    /*
+        GETTERS
+    */
+
+    public String getNome() {
+        return nome;
+    }
+
+    public HashMap<String, Curso> getCursosAssociados() {
+        return cursosAssociados;
+    }
+
+    public Professor getDiretor() {
+        return diretor;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /*
+        SETTERS
+    */
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCursosAssociados(Curso cursoAssociado) {
+        this.cursosAssociados.put(cursoAssociado.getNome(), cursoAssociado);
+    }
+
+    public void setDiretor(Professor diretor) {
+        this.diretor = diretor;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
 }
