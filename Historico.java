41517105b5b3dca1f8d366c929f4fec7f9af44ea
Guilherme <guilhermesergiooliveira55@gmail.com>
2021-04-02 22:57:30 -0300

    basico Instituicao e Historico

---
+++ Historico.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import entities.Disciplina;
+
+public class Historico {
+    private HashMap<String, Disciplina> disciplinasCursadas;
+    private ArrayList<Double> notas;
+    private String semestre;
+    private double coeficiente;
+    private Curso grade; //Curso?
+
+    public Historico(ArrayList<Double> notas, String semestre, double coeficiente, Curso grade){
+        this.notas = notas;
+        this.semestre = semestre;
+        this.coeficiente = coeficiente;
+        this.grade = grade;
+    }
+    
+
+    /* Getters  */
+    public HashMap<String, Disciplina> getDisciplinasCursadas() {
+        return disciplinasCursadas;
+    }
+
+    public ArrayList<Double> getNotas() {
+        return notas;
+    }
+    
+    
+    public String getSemestre() {
+        return semestre;
+    }
+    
+    public double getCoeficiente() {
+        return coeficiente;
+    }
+
+    public Curso getGrade() {
+        return this.grade;    
+    }
+    
+    /* Setters */
+    public void setDisciplinasCursadas(HashMap<String, Disciplina> disciplinasCursadas) {
+        this.disciplinasCursadas = disciplinasCursadas;
+    }
+
+    public void setNotas(ArrayList<Double> notas) {
+        this.notas = notas;
+    }    
+    
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+    
+    public void setCoeficiente(double coeficiente) {
+        this.coeficiente = coeficiente;
+    }
+
+    public void setGrade(Curso grade){
+        this.grade = grade;
+    }
+}
--- Instituicao.java
+++ Instituicao.java
@@ -1,3 +1,67 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+import entities.Disciplina;
+
 public class Instituicao {
+    private String nome;
+    private HashMap<String, Disciplina> disciplinas; //Disciplina? 
+    private HashMap<String, Professor> professores;
+    private HashMap<String, Curso> cursos;
+    private HashMap<String, Departamento> departamentos;
+
+
+    public Instituicao(String nome){
+        this.nome = nome;
+    }
+
+
+    /* Getters */
+    public String getNome(){
+        return this.nome;
+    }
+
+    public HashMap<String, Disciplina> getDisciplina(){
+        return this.disciplinas;
+    }
+
+    public HashMap<String, Professor> getProfessores(){
+        return this.professores;
+    }
+
+    public HashMap<String, Curso> getCursos(){
+        return this.cursos;
+    }
+
+    public HashMap<String, Departamento> getDepartamentos(){
+        return this.departamentos;
+    }
+
+
+
+    /* Setters */
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setDisciplina(HashMap<String, Disciplina> disciplinas){
+        this.disciplinas = disciplinas;
+    }
+
+    public void setProfessores(HashMap<String, Professor> professores){
+        this.professores = professores;
+    }
+
+    public void  setCursos(HashMap<String, Curso> cursos){
+        this.cursos = cursos;
+    }
+
+    public void setDepartamentos(HashMap<String, Departamento> departamentos){
+        this.departamentos = departamentos;
+    }
+
+
+    
+
     
 }
